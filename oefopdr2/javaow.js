572a2cf474db8e75e13f46b40cf0a01cee038b67
Jessikxa <iamjessicadurkin@gmail.com>
2025-01-21 13:57:57 +0100

    21/01/25

---
+++ oefopdr2/javaow.js
@@ -0,0 +1,35 @@
+
+// i need this to become array of letters . dan kan ik zeggen livesLeft  == guesswordArray (sorta because we dont want it to win if livesLeft == guesswordarray)
+// we want it to equal the letter in the word nog the word itself. 
+// console.log(guessWord);
+
+//how to have streepjes of length of word show up on screen when player typed in their word of choice? 
+
+
+// get tha shtuff you do know in theru
+
+// let guessLetter = prompt("hi pick a word");
+// let gameOver = prompt("Game Over!");
+// let correctLetter; //boolean
+// if boolean(correctLetter) = false { livesLeft--}
+// // if boolean(correctLetter) = true {letter shows up on screen} ---> how letters show up on screen ?
+// let livesLeft = 10; 
+// // if (livesLeft == 0){gameOver}
+
+// if(something that only indicates a WIN if ALL letters of the word are guessed )
+// let winAlert = alert("you guessed the word!");
+
+// if(livesLeft == 0){
+//     let loseAlert = alert("all lives lost. you lose");
+// }
+
+// nog een ding dat guessword veranderd in streepjes ----  guessword == wordStreepje[]
+
+
+// document.write(guessWord)
+
+// function FunCtiON(){
+//     console.log (5*6);
+// }
+
+ document.getElementById(weewop)
