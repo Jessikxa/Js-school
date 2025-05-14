89fec09cffbe50795a1d0164bbd24fd4fa4333c4
Jessikxa <iamjessicadurkin@gmail.com>
2025-05-14 13:15:07 +0200

    - added a placeholder to let people know what to fill in. - lives count down working() - only allowed to type one character - error message when nothing filled in and presses button. not allowed to fill in blank - resets text field after guessing a letter

--- Galgje/javaow.js
+++ Galgje/javaow.js
@@ -3,8 +3,13 @@ const exampleStrong = document.getElementById("wordPickk");
 // console.log(lengthOfString);
 const btn = document.getElementById("btn");
 const displo = document.getElementById("displo");
+const Lives = document.getElementById("Lives");
+const Text = document.getElementById("wordPickk");
 
-word = exampleStrong.value
+var wordToGuess = "";
+var numberOfLives = 10;
+
+// word = exampleStrong.value
 
 function WordToDash(){
     const lengthOfString = exampleStrong.value.length;
@@ -13,13 +18,62 @@ function WordToDash(){
     displo.innerHTML = wordDasg + " " + lengthOfString;
 }
 
-btn.addEventListener('click' ,fun1);
+// btn.addEventListener('click' ,fun1);
+
+function guessesLeft()
+{
+
+        numberOfLives--
+        Lives.textContent = `Lives left: ${numberOfLives}`;
+    
+        if(numberOfLives < 0)
+            {
+                numberOfLives = 0;
+                Lives.textContent = `Lives left: ${numberOfLives}`;
+                displo.textContent = "You lose. Try again?";
+            }
+           
+        return; //need a try again button that appears after number of lives hits 0 that restarts the game
+    
+    
+}
+
+// btn.addEventListener("click", event => {
+//     numberOfLives--
+//     Lives.textContent = `Lives left: ${numberOfLives}`;
+
+//     if(numberOfLives < 0)
+//         {
+//             numberOfLives = 0;
+//             Lives.textContent = `Lives left: ${numberOfLives}`;
+//             displo.textContent = "You lose. Try again?";
+//         }
+      
+//     return Lives + displo;  //need a try again button that appears after number of lives hits 0 that restarts the game
+// })
+
+function IsEmpty() {
+    if (document.forms['form'].wordPickk.value === "") {
+      alert("empty");
+      return false;
+    }
+    else{console.log(wordPickk.value);}
+    guessesLeft();
+    document.getElementById("wordPickk").value = ""
+    return true;
+}
+
+// getElementById("wordPickk").textContent = "";
 
 
-
+// function resetForm()
+// {
+//     var Form = document.forms['form'].reset;
+//     Form.reset();
+// }
 //  let guessingGame = document.getElementById(guessingGame);
 
 //  guessingGame.addEventListener("submit", (event) =>{
 //     event.preventDefault();
 //     const xyz = ${event: value};
-//  });
+//  })
